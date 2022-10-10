e080a32520cd4d8eb5b6bdc788aa3933762f1473
jacob3015 <jacob3015@gmail.com>
2022-10-11 05:29:54 +0900

    Init App

---
+++ src/main/java/kr/pe/aichief/AichiefSpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.aichief;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AichiefSpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AichiefSpringbootBackendApplication.class, args);
+	}
+
+}
