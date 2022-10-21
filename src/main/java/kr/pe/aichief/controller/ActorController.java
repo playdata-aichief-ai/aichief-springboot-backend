faad5dc3f333b000a638741efe1cee3538e92a68
jacob3015 <jacob3015@gmail.com>
2022-10-21 10:08:12 +0900

    Fix cors problem

--- src/main/java/kr/pe/aichief/controller/ActorController.java
+++ src/main/java/kr/pe/aichief/controller/ActorController.java
@@ -2,6 +2,7 @@ package kr.pe.aichief.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ import kr.pe.aichief.model.service.ActorService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
+@CrossOrigin(origins = "https://main.ddnum7vb34gxw.amplifyapp.com")
 @RequestMapping("/search")
 @RequiredArgsConstructor
 public class ActorController {
