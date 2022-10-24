b1c34f6b7bbc1d993cd33007358c3cb808db9f87
jacob3015 <jacob3015@gmail.com>
2022-10-24 10:51:06 +0900

    Update web configuration added allow credentials

--- src/main/java/kr/pe/aichief/config/WebConfig.java
+++ src/main/java/kr/pe/aichief/config/WebConfig.java
@@ -12,9 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins(
-					"https://main.ddnum7vb34gxw.amplifyapp.com",
-					"https://dev.ddnum7vb34gxw.amplifyapp.com"
-			);
+			.allowedOrigins("https://dev.ddnum7vb34gxw.amplifyapp.com")
+			.allowCredentials(true);
 	}
 }
