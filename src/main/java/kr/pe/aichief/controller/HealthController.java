a9353874e677393e0c59e378e841fbd7b7f412cd
jacob3015 <jacob3015@gmail.com>
2022-10-24 11:25:51 +0900

    Update health check for application load balancer target group

--- src/main/java/kr/pe/aichief/controller/HealthController.java
+++ src/main/java/kr/pe/aichief/controller/HealthController.java
@@ -1,16 +1,17 @@
 package kr.pe.aichief.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
-public class TestController {
+@RequestMapping("/")
+public class HealthController {
 	
 	@GetMapping
-	public String hello(@RequestParam("name") String name) {
-		return String.join(" ", name, "Test Success");
+	public ResponseEntity<HttpStatus> healthCheck() {
+		return new ResponseEntity<HttpStatus>(HttpStatus.OK);
 	}
 }
